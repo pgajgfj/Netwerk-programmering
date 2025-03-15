0aa305b7886ddee6f289088592d10cdc750e5547
pgajgfj <121456433+pgajgfj@users.noreply.github.com>
2025-03-15 14:25:58 +0200

    Add files via upload

---
+++ TicTacToeServer2404/TicTacToeClient/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TicTacToeClient
+{
+    public partial class MainWindow : Window
+    {
+        private TcpClient _client;
+        private NetworkStream _stream;
+        private string _player = "X";
+        private string _board = "         ";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _client = new TcpClient("127.0.0.1", 12345);
+            _stream = _client.GetStream();
+            ReceiveGameUpdates();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            int index = int.Parse(button.Name.Substring(6));
+            if (_board[index] == ' ')
+            {
+                _board = _board.Substring(0, index) + _player + _board.Substring(index + 1);
+                UpdateButtons();
+                SendMove(index);
+                CheckGameStatus();
+                TogglePlayer();
+            }
+        }
+
+        private void SendMove(int index)
+        {
+            byte[] message = Encoding.UTF8.GetBytes(index.ToString());
+            _stream.Write(message, 0, message.Length);
+        }
+
+        private void UpdateButtons()
+        {
+            button0.Content = _board[0].ToString();
+            button1.Content = _board[1].ToString();
+            button2.Content = _board[2].ToString();
+            button3.Content = _board[3].ToString();
+            button4.Content = _board[4].ToString();
+            button5.Content = _board[5].ToString();
+            button6.Content = _board[6].ToString();
+            button7.Content = _board[7].ToString();
+            button8.Content = _board[8].ToString();
+        }
+
+        private void TogglePlayer()
+        {
+            _player = _player == "X" ? "O" : "X";
+        }
+
+        private void ReceiveGameUpdates()
+        {
+            byte[] buffer = new byte[256];
+            _stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(ReceiveCallback), buffer);
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            byte[] buffer = (byte[])ar.AsyncState;
+            int bytesRead = _stream.EndRead(ar);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Dispatcher.Invoke(() => gameStatus.Text = message);
+            ReceiveGameUpdates();
+        }
+
+        private void CheckGameStatus()
+        {
+            string[] winningCombinations = {
+                "012", "345", "678",
+                "036", "147", "258",
+                "048", "246"
+            };
+
+            foreach (var combination in winningCombinations)
+            {
+                char a = _board[combination[0] - '0'];
+                char b = _board[combination[1] - '0'];
+                char c = _board[combination[2] - '0'];
+                if (a == b && b == c && a != ' ')
+                {
+                    gameStatus.Text = _player == "X" ? "Виграв X!" : "Виграв O!";
+                    MessageBox.Show($"{_player} виграв! Гра почнеться знову.");
+                    ResetGame();
+                    return;
+                }
+            }
+
+            if (_board.IndexOf(' ') == -1)
+            {
+                gameStatus.Text = "Нічия!";
+                MessageBox.Show("Нічия! Гра почнеться знову.");
+                ResetGame();
+            }
+        }
+
+        private void ResetGame()
+        {
+            _board = "         ";
+            UpdateButtons();
+            gameStatus.Text = "Нова гра почалась!";
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ TicTacToeServer2404/TicTacToeServer2404/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class TicTacToeServer
+{
+    private static TcpListener listener;
+    private static TcpClient client1;
+    private static TcpClient client2;
+    private static NetworkStream stream1;
+    private static NetworkStream stream2;
+
+    static void Main()
+    {
+        listener = new TcpListener(IPAddress.Any, 12345);
+        listener.Start();
+        Console.WriteLine("Сервер запущений, чекаємо на підключення...");
+
+        client1 = listener.AcceptTcpClient();
+        stream1 = client1.GetStream();
+        Console.WriteLine("Клієнт 1 підключений.");
+
+        client2 = listener.AcceptTcpClient();
+        stream2 = client2.GetStream();
+        Console.WriteLine("Клієнт 2 підключений.");
+
+        Thread gameThread = new Thread(PlayGame);
+        gameThread.Start();
+    }
+
+    private static void PlayGame()
+    {
+        string board = "         ";
+        string turn = "X";
+        while (true)
+        {
+            board = "         ";  
+            SendMessage(stream1, "Нова гра почалась! Ваш хід. Грайте X: " + board);
+            SendMessage(stream2, "Нова гра почалась! Очікуйте хід X: " + board);
+
+            while (true)
+            {
+                int move1 = GetMove(stream1);
+                board = MakeMove(board, move1, "X");
+                if (CheckWin(board))
+                {
+                    SendMessage(stream1, "Виграв X! Гра завершена.");
+                    SendMessage(stream2, "Виграв X! Гра завершена.");
+                    break;
+                }
+
+                SendMessage(stream1, "Очікуйте хід O: " + board);
+                SendMessage(stream2, "Ваш хід. Грайте O: " + board);
+
+                int move2 = GetMove(stream2);
+                board = MakeMove(board, move2, "O");
+                if (CheckWin(board))
+                {
+                    SendMessage(stream1, "Виграв O! Гра завершена.");
+                    SendMessage(stream2, "Виграв O! Гра завершена.");
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void SendMessage(NetworkStream stream, string message)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(message);
+        stream.Write(buffer, 0, buffer.Length);
+    }
+
+    private static int GetMove(NetworkStream stream)
+    {
+        byte[] buffer = new byte[256];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        return int.Parse(message);
+    }
+
+    private static string MakeMove(string board, int position, string player)
+    {
+        char[] boardArray = board.ToCharArray();
+        boardArray[position] = player[0];
+        return new string(boardArray);
+    }
+
+    private static bool CheckWin(string board)
+    {
+        string[] winningCombinations = {
+            "012", "345", "678",
+            "036", "147", "258",
+            "048", "246"
+        };
+
+        foreach (var combination in winningCombinations)
+        {
+            char a = board[combination[0] - '0'];
+            char b = board[combination[1] - '0'];
+            char c = board[combination[2] - '0'];
+            if (a == b && b == c && a != ' ')
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
